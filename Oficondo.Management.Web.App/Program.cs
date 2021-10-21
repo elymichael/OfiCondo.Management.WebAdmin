8998e8adb5c5dbf7caa938707ef8a95c5593ee9f
elymichael <elymichael@gmail.com>
2021-10-21 18:13:07 -0400

    Upload changes application

--- Oficondo.Management.Web.App/Program.cs
+++ Oficondo.Management.Web.App/Program.cs
@@ -1,21 +1,16 @@
-using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Oficondo.Management.Web.App.Auth;
-using Oficondo.Management.Web.App.Contracts;
-using Oficondo.Management.Web.App.Services;
-using Oficondo.Management.Web.App.Services.Base;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Oficondo.Management.Web.App
 {
+    using Blazored.LocalStorage;
+    using Microsoft.AspNetCore.Components.Authorization;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Oficondo.Management.Web.App.Auth;
+    using Oficondo.Management.Web.App.Contracts;
+    using Oficondo.Management.Web.App.Services;
+    using Oficondo.Management.Web.App.Services.Base;
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
     public class Program
     {
         private const string apiUrl = "https://localhost:44312";
